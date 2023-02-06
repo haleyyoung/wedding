0fb3aa24f5c265513da16c83645dbb4b7b1c634e
Haley Young <haleyyoung88@gmail.com>
2023-02-05 21:49:13 -0800

    Nav colors

--- src/GlobalNavbar.js
+++ src/GlobalNavbar.js
@@ -10,12 +10,17 @@ export default function GlobalNavbar() {
   };
 
   return (
-    <Box>
-      <Tabs value={tabIndex} onChange={handleTabChange}>
-        <Tab label="What's Happening?" />
-        <Tab label="What To Bring" />
-        <Tab label="The Day Of" />
-        <Tab label="Registry" />
+    <Box className="nav">
+      <Tabs value={tabIndex} onChange={handleTabChange}
+          sx={{
+            background: 'rgb(121,9,111)',
+            background: 'linear-gradient(139deg, rgba(121,9,111,1) 0%, rgba(208,128,24,1) 86%, rgba(255,192,0,1) 100%)',
+            color: 'white',
+          }}>
+        <Tab label="What's Happening?" sx={{color: 'white'}} />
+        <Tab label="What To Bring" sx={{color: 'white'}} />
+        <Tab label="The Day Of" sx={{color: 'white'}} />
+        <Tab label="Registry" sx={{color: 'white'}} />
       </Tabs>
     </Box>
   );
