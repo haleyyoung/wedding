0ae5f21d86f4a21609b2a8fb6942f56219d7d22d
Haley Young <haleyyoung88@gmail.com>
2023-04-28 07:02:02 -0700

    Start styling day-of page

--- src/DayOf.jsx
+++ src/DayOf.jsx
@@ -7,17 +7,21 @@ export default function DayOf() {
   const ref = useRef();
   console.log("in day of??");
   return (
-    <Parallax className="parallax" pages={3} ref={ref} style={{background: '#000033'}}>
-      <ParallaxLayer className="content" offset={0} speed={0.1} style={{zIndex: 9999}}>
-        <Box sx={{color: "white"}}>
-          7am - 7:45am
-          Hunter will be at the gas station exiting June Lake with extra copies
-          of the paperwork you'll need to get in to the park if you forgot to print
-          them before leaving.
-          <Link href="https://goo.gl/maps/vCu7FjU1ZgniXATP9">Google maps</Link>
+    <Parallax className="parallax day-of" pages={3} ref={ref} style={{background: '#000033'}}>
+      <ParallaxLayer className="text" offset={0} speed={0.1} style={{zIndex: 9999}}>
+        <Box sx={{display: 'flex', gap: '20px', padding: '3em', textAlign: 'left'}}>
+          <div className="time" style={{color: "white", flex: 1}}>
+            7am - 7:45am
+          </div>
+          <div className="text" style={{flex: 3}}>
+            Hunter will be at the gas station exiting June Lake with extra copies
+            of the paperwork you'll need to get in to the park if you forgot to print
+            them before leaving.
+            <Link href="https://goo.gl/maps/vCu7FjU1ZgniXATP9">Google maps</Link>
+          </div>
         </Box>
       </ParallaxLayer>
-      <ParallaxLayer className="content" offset={1} speed={0.1} style={{zIndex: 9999}}>
+      <ParallaxLayer className="text" offset={1} speed={0.1} style={{zIndex: 9999}}>
         <Box sx={{color: "white"}}>
           8am - 9am
           Other stuff
