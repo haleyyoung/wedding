7e3484e5c7c44ea21be6415233ea217c6c01931c
Haley Young <haleyyoung88@gmail.com>
2023-06-13 21:00:30 -0700

    Wedding party and day of page styling cleanup

--- src/WeddingParty.jsx
+++ src/WeddingParty.jsx
@@ -6,50 +6,48 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 export default function YosemiteRules() {
   const ref = useRef();
   return (
-    <Parallax className="parallax wedding-party" pages={3} ref={ref} style={{background: '#000033'}}>
-      <ParallaxLayer className="text" offset={0} speed={0.1} style={{zIndex: 9999}}>
-        <Box>
-          <h1>Important Dates</h1>
-          <div className="event">
-            <h2>Wedding Dress Shopping</h2>
-            <p className="note"> (bridesmaids only)</p>
-            <div className="date">
-              Friday and Saturday August 26/27, 2023<br/>
-              San Diego
-            </div>
+    <div className="parallax wedding-party" style={{background: '#000033'}}>
+      <Box>
+        <h1>Important Dates</h1>
+        <div className="event">
+          <h2>Wedding Dress Shopping</h2>
+          <p className="note"> (bridesmaids only)</p>
+          <div className="date">
+            Friday and Saturday August 26/27, 2023<br/>
+            San Diego
           </div>
-          <div className="event">
-            <h2>Rehearsal</h2>
-            <div className="date">
-              Friday July 12, 2024 - 12pm<br/>
-              Gull Lake
-            </div>
+        </div>
+        <div className="event">
+          <h2>Rehearsal</h2>
+          <div className="date">
+            Friday July 12, 2024 - 12pm<br/>
+            Gull Lake
           </div>
-          <div className="event">
-            <h2>Wedding!</h2>
-            <div className="date">
-              Saturday July 13, 2024 - 5am - 11am<br/>
-              Yosemite - Tenaya Lake
-            </div>
+        </div>
+        <div className="event">
+          <h2>Wedding!</h2>
+          <div className="date">
+            Saturday July 13, 2024 - 5am - 11am<br/>
+            Yosemite - Tenaya Lake
           </div>
-          <div className="event">
-            <h2>Brunch</h2>
-            <div className="date">
-              Sunday July 14, 2024 - 10am<br/>
-              Gull Lake
-            </div>
+        </div>
+        <div className="event">
+          <h2>Brunch</h2>
+          <div className="date">
+            Sunday July 14, 2024 - 10am<br/>
+            Gull Lake
           </div>
-        </Box>
-      </ParallaxLayer>
-      <ParallaxLayer className="text" offset={1} speed={0.1} style={{zIndex: 9999}}>
-        <Box>
-          <h1>Lodging</h1>
-          <img src={process.env.PUBLIC_URL + "/airbnb.png"}/>
-          <p>The wedding party and immediate family will be staying in a couple AirBnbs we've reserved on Gull Lake, so no need to worry about lodging.</p>
-          <p>Each couple will have their own bedroom and bathroom</p>
-          <p>No pets, unfortunately</p>
-        </Box>
-      </ParallaxLayer>
-    </Parallax>
+        </div>
+      </Box>
+      <Box>
+        <h1>Lodging</h1>
+        <img src={process.env.PUBLIC_URL + "/airbnb.png"}/>
+        <p>The wedding party and immediate family will be staying in a
+        <Link href="https://goo.gl/maps/QBHNDEhc6tSeq6zz7" target="_blank" color="secondary" underline="none"> couple AirBnbs </Link>
+        we've reserved on Gull Lake, so no need to worry about lodging.</p>
+        <p>Each couple will have their own bedroom and bathroom.</p>
+        <p>No pets, unfortunately.</p>
+      </Box>
+    </div>
   );
 };
