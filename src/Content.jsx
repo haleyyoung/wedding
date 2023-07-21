bdf7e7bb0f3df10d35e5019117bcd38e74be8bdc
Haley Young <haleyyoung88@gmail.com>
2023-07-21 01:00:33 -0700

    Homepage draft working

--- src/Content.jsx
+++ src/Content.jsx
@@ -15,41 +15,29 @@ import DayOf from './DayOf.jsx';
 export default function Content() {
   const ref = useRef()
   return (
-    <Parallax className="parallax" pages={1} ref={ref} config={
-      {
-        mass: 5,
-        tension: 180,
-        friction: 180,
-      }}
+    <Parallax className="parallax App" pages={3} ref={ref}
     >
-      <ParallaxLayer sticky={{start: 0, end: 0.5}}>
-        <ParallaxLayer offset={0} style={{zIndex: 4}} speed={1.1}>
+      <ParallaxLayer sticky={{start: 0, end: 0.75}}>
+        <ParallaxLayer offset={0} style={{zIndex: 4}} speed={0.8}>
           <div className="image-1"></div>
           <div className="navy-background content">
             <div className="text">
-              Tenaya Lake, Yosemite <br/> July 13, 2024 <br/> 9am
+              July 13, 2024 <br/>
+              Tenaya Lake, Yosemite <br/>
+              9am <br/> <br/>
+              June Lake <br/>
+              8pm <br/>
             </div>
-            <div className="disclaimer">
-              We're going on an adventure for our wedding, and we'd love if you'd
-              join us, but we also know that this is by no means a traditional
-              wedding. Please checkout the pages here for all the info, and if -
-              for whatever reason - you'd rather not come, we encourage you to
-              make that choice for yourself, knowing there will be no hard feelings
-              on our end.
-              <br/>
-              ~ Haley and Garrison
-            </div>
-            <button onClick={() => ref.current.scrollTo(0)}>Scroll to top</button>
           </div>
-          <div className="tenaya"></div>
+          <div className="navy-background content"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} style={{zIndex: 3}} speed={0.6}>
+        <ParallaxLayer offset={0} style={{zIndex: 3}} speed={0.3}>
           <div className="image-2"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} style={{zIndex: 2}} speed={0.3}>
+        <ParallaxLayer offset={0} style={{zIndex: 2}} speed={0.1}>
           <div className="image-3"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} style={{zIndex: 1}} speed={0.1}>
+        <ParallaxLayer offset={0} style={{zIndex: 1}} speed={0.01}>
           <div className="image-4"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0.1} className="content" speed={-0.2} style={{zIndex: 2}}>
@@ -66,6 +54,23 @@ export default function Content() {
             <span style={{display: 'block'}}>
               invite you to join them!
             </span>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.49} style={{zIndex: 6}} speed={0.01}>
+          <div className="tenaya"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.49} style={{background: 'transparent', height: '25%', marginTop: '55%', zIndex: 7}} speed={0.2}>
+          <div className="content">
+            <div className="disclaimer">
+              We're going on an adventure for our wedding, and we'd love if you'd
+              join us, but we also know that this is, by no means, a traditional
+              wedding. Please check out the pages here for all the info, and if -
+              for whatever reason - you'd rather not come, we encourage you to
+              make that choice for yourself, knowing there will be no hard feelings
+              on our end.
+              <br/>
+              ~ Haley and Garrison
+            </div>
           </div>
         </ParallaxLayer>
       </ParallaxLayer>
